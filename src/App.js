0b6ff8765de60ac604f64f12c5f9784e9791d9ae
gomex500 <gomezfreddy886@gmail.com>
2023-07-06 17:47:58 -0600

    guardado axuiliar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import Navigation from './Navigation';
 
 const App = () => {
     return(
-        <View>
-            <Text>Hola</Text>
-        </View>
-    )
-}
+        <Navigation/>
+    );
+};
 
 export default App;
